1527e0b848d9e959d0de22db4666f6b73ca743a5
Nikolay-MA <nikolay02092008@gmail.com>
2026-09-23 12:31:45 +0300

    МИРЭА - Российский технологический университет

--- src/components/MireaStudyPage.jsx
+++ src/components/MireaStudyPage.jsx
@@ -5,7 +5,7 @@ export default function MireaStudyPage() {
     <section className="info-section-block page-fade-animation">
       <h2>Учеба в МИРЭА</h2>
       <div className="legal-info-card">
-        <h3>РТУ МИРЭА — ИКиЦТ</h3>
+        <h3>МИРЭА - Российский технологический университет</h3>
         <p>Институт кибербезопасности и цифровых технологий.</p>
         <p>Направление: Искусственный Интеллект & Анализ Данных.</p>
       </div>
